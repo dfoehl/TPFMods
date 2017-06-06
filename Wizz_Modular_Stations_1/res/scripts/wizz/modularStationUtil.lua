1fd5e43b51b6755c22844a63f775c9c3ef915000
dfoehl <dominik@xn--fhl-online-ecb.de>
2017-06-06 23:43:30 +0200

    /ModularStationsV1.0: Auto stash before merge of "release/ModularStationsV1.0" and "develop"
    Created first "dummy" construction and initial version of script file.

--- Wizz_Modular_Stations_1/res/scripts/wizz/modularStationUtil.lua
+++ Wizz_Modular_Stations_1/res/scripts/wizz/modularStationUtil.lua
@@ -1,3 +1,21 @@
-
-
 local modularStationUtil = {}
+
+
+function modularStationUtil.makeTrainStationParams()
+	return {
+		{
+			key = "cargoType",
+			name = _("Passenger/Cargo"),
+			values = { _("Passenger"), _("Cargo") },
+			yearFrom = 0,
+			yearTo = 0
+		},
+		{
+			key = "trackType",
+			name = _("Track type"),
+			values = { _("Standard"), _("High-speed") },
+			yearFrom = 1925,
+			yearTo = 0
+		},
+	}
+end
