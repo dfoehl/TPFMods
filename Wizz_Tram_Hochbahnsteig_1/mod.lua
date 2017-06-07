867b2468974b92c8ebe241b8e613b9560c1b45ef
dfoehl <dominik@xn--fhl-online-ecb.de>
2017-06-07 23:20:55 +0200

    Streets get loaded for paramters of .cons

--- Wizz_Tram_Hochbahnsteig_1/mod.lua
+++ Wizz_Tram_Hochbahnsteig_1/mod.lua
@@ -1,25 +1,41 @@
+﻿streets = {}
+
 function data()
-  return {
-  
-	info = {
-		minorVersion = 3,
-		severityAdd = "NONE",
-		severityRemove = "WARNING", 
-		name = _("wizzPlatforms.mod.name"),
-		description = _("wizzPlatforms.mod.desc"),
-		tags = { "Tram Station" },
-		authors = {
-			{
-				name = 'WiZz4Rd',
-				role = 'CREATOR',
-				steamProfile = '76561197969534057',
-				tfnetId = 26343
-			},
+
+	local function myModifier(fileName, data)
+		print("Neue Straße hinzugefügt: "..fileName)
+		if data.name then
+			print("Name: "..data.name)
+			print("Streetwidth: "..data.streetWidth)
+			streets[#streets+1] = { name = data.name, width = data.streetWidth }
+		end
+		for k, v in pairs(data) do
+			print(k)
+		end
+		print("Streets: "..#streets)
+		print("----------------")
+		return data
+	end
+ 
+	addModifier("loadStreet", myModifier)
+
+	return {
+		info = {
+			minorVersion = 4,
+			severityAdd = "NONE",
+			severityRemove = "WARNING", 
+			name = _("wizzPlatforms.mod.name"),
+			description = _("wizzPlatforms.mod.desc"),
+			tags = { "Tram Station" },
+			authors = {
+				{
+					name = 'WiZz4Rd',
+					role = 'CREATOR',
+					steamProfile = '76561197969534057',
+					tfnetId = 26343
+				},
+			},			
+			visible = true,			
 		},
-		
-		visible = true,
-		
-	},
-  
-  }
+	}
 end
